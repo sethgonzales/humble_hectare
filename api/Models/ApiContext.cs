11cee27aedd05d552defe98dd4f78796e4c8789c
sethgonzales <sethgonzales157@gmail.com>
2024-02-23 21:13:07 -0800

    create crop model and migration

---
+++ api/Models/ApiContext.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Models
+{
+  public class ApiContext : DbContext
+  {
+    public DbSet<Crop> Crops { get; set; }
+
+    public ApiContext(DbContextOptions<ApiContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Crop>()
+        .HasData(
+          new Crop { CropId = 1, Name = "Tomato" },
+          new Crop { CropId = 2, Name = "Grape" },
+          new Crop { CropId = 3, Name = "Pear" },
+          new Crop { CropId = 4, Name = "Blackberry" }
+        );
+    }
+  }
+}
---
+++ api/Models/Crop.cs
@@ -0,0 +1,9 @@
+namespace Api.Models
+
+{
+  public class Crop
+  {
+    public int CropId { get; set; }
+    public string Name { get; set; }
+  }
+}
